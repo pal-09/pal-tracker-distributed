f59a6c0276fed576867db018df76aa28f3aa54c8
Hananiel <hananiel@gmail.com>
2017-10-09 14:21:29 -0400

    oauth lab

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,15 +1,21 @@
 package io.pivotal.pal.tracker.allocations;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+@Component
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
     private ConcurrentHashMap<Long, ProjectInfo> cacheMap = new ConcurrentHashMap<Long, ProjectInfo>();
+//    @Autowired
+//    private RedisTemplate redisTemplate;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
@@ -21,11 +27,14 @@ public class ProjectClient {
     public ProjectInfo getProject(long projectId) {
         ProjectInfo info = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
         cacheMap.put(projectId, info);
+//        redisTemplate.boundListOps(projectId).leftPush(info);
+//        redisTemplate.opsForValue().set(projectId,info);
         return info;
     }
 
     public ProjectInfo getProjectFromCache(long projectId)
     {
         return cacheMap.get(projectId);
+//        return (ProjectInfo) redisTemplate.opsForValue().get(projectId);
     }
 }
